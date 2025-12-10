38e239cdaf2be8bd447f41c13845ab5bceddc476
PrasiddhShah <prasiddhshah.18@gmail.com>
2025-12-09 21:11:13 -0800

    circle class added

---
+++ circleshape.py
@@ -0,0 +1,18 @@
+import pygame
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self,x,y,radius):
+        # we will be using this later
+        if hasattr(self,"containers"):
+            super().__init__(self.container)
+        else:
+            super().__init__()
+        self.postion = pygame.Vector2(x,y)
+        self.velocity = pygame.Vector2(0,0)
+        self.radius = radius
+    
+    def draw(self, screen):
+        # must override
+        pass
+    def update(self,dt):
+        # must override
+        pass
